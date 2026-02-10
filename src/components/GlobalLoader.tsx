5ded37eb82c28e8d78ce2654c3cec0cf6c6fbc1b
yasir5405 <yasirnaseem5415@gmail.com>
2026-02-10 21:00:38 +0530

    Fix global loader

--- src/components/GlobalLoader.tsx
+++ src/components/GlobalLoader.tsx
@@ -5,10 +5,10 @@ const GlobalLoader = () => {
     <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-black/10 backdrop-blur-sm">
       <div className="border border-muted-foreground p-1 rounded-full shadow-md">
         <Spinner stroke="#ff7e5f" />
-        <h1 className="text-xs md:text-sm text-primary">
-          Loading...Please wait...
-        </h1>
       </div>
+      <h1 className="text-xs md:text-sm text-primary">
+        Loading...Please wait...
+      </h1>
     </div>
   );
 };
