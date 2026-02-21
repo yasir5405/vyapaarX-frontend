7e87c1dc549252a683f45f2f9f8284c6887010e6
yasir5405 <yasirnaseem5415@gmail.com>
2026-02-22 03:58:42 +0530

    Add bottom navbar on mobile

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,21 @@
 import { getHomeProducts, type HomeProducts } from "@/api/product.api";
 import HomePageCarousel from "@/components/carousel/HomePageCarousel";
 import HomeSection from "@/components/HomeSection";
+import { House, Search, Sparkle, User, type LucideIcon } from "lucide-react";
 import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 
+interface BottomLinks {
+  name: string;
+  icon: LucideIcon | string;
+  href: string;
+}
+
 const Home = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const pathname = location.pathname;
   const [products, setProducts] = useState<HomeProducts | null>(null);
 
   useEffect(() => {
@@ -29,8 +40,15 @@ const Home = () => {
     fetchProducts();
   }, []);
 
+  const bottomLinks: BottomLinks[] = [
+    { name: "Home", icon: House, href: "/" },
+    { name: "Browse", icon: Search, href: "/my/orders" },
+    { name: "Beauty", icon: Sparkle, href: "/my/orders" },
+    { name: "Profile", icon: User, href: "/my" },
+  ];
+
   return (
-    <div className="w-full">
+    <div className="w-full relative">
       <HomePageCarousel />
 
       <div className="w-full py-4 flex flex-col gap-4 items-center mt-3">
@@ -41,16 +59,50 @@ const Home = () => {
       </div>
 
       {products && (
-        <HomeSection products={products?.newArrivals} title="New Arrivals" desc="Discover the latest trends and fresh styles just added to our collection" />
+        <HomeSection
+          products={products?.newArrivals}
+          title="New Arrivals"
+          desc="Discover the latest trends and fresh styles just added to our collection"
+        />
       )}
 
       {products && (
-        <HomeSection products={products?.menProducts} title="Men's" desc="Explore our curated collection of men's fashion and accessories" />
+        <HomeSection
+          products={products?.menProducts}
+          title="Men's"
+          desc="Explore our curated collection of men's fashion and accessories"
+        />
       )}
 
       {products && (
-        <HomeSection products={products?.womenProducts} title="Women's" desc="Browse elegant styles and trending fashion for women" />
+        <HomeSection
+          products={products?.womenProducts}
+          title="Women's"
+          desc="Browse elegant styles and trending fashion for women"
+        />
       )}
+
+      <div className="w-full fixed left-0 bottom-0 border-t bg-white flex items-center justify-between md:hidden shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1)] h-16 px-5">
+        {bottomLinks.map(({ href, icon: Icon, name }: BottomLinks, index) => {
+          const isActive = pathname === href;
+          return (
+            <div
+              className={`flex h-full relative flex-col items-center justify-center gap-1 after:content-[''] after:absolute after:left-1/2 after:-translate-x-1/2 after:top-0 ${isActive ? "after:bg-primary" : "after:bg-transparent"} after:h-0.5 ${pathname === "/" || pathname === "/my/profile" ? "after:w-[210%]" : "after:w-[160%]"}`}
+              onClick={() => navigate(href)}
+              key={index}
+            >
+              <Icon
+                className={`size-5 ${isActive ? "text-primary" : ""}`}
+                strokeWidth={1.5}
+              />
+
+              <h1 className={`text-sm ${isActive ? "text-primary" : ""}`}>
+                {name}
+              </h1>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
